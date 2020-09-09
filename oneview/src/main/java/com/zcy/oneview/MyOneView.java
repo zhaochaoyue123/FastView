9d0b2af7c56a46dfd6ffe336496a433ff24c95a4
zhaochaoyue <770812088@qq.com>
2020-09-09 22:23:14 +0800

    Initial commit

---
+++ oneview/src/main/java/com/zcy/oneview/MyOneView.java
@@ -0,0 +1,21 @@
+package com.zcy.oneview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MyOneView extends View {
+    public MyOneView(Context context) {
+        super(context);
+    }
+
+    public MyOneView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyOneView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ twoview/src/main/java/com/zcy/twoview/MyTwoView.java
@@ -0,0 +1,21 @@
+package com.zcy.twoview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MyTwoView extends View {
+    public MyTwoView(Context context) {
+        super(context);
+    }
+
+    public MyTwoView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTwoView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
